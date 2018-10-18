9c44c0080213f0807d15d9e94b726348ab0508d4
shroyer.shawn@gmail.com <41597227+ncredsquirrel@users.noreply.github.com>
2018-10-17 23:52:15 -0400

    Added contact me and linked up new projects and bug fixes

--- DayThree_Project/Controllers/HomeController.cs
+++ DayThree_Project/Controllers/HomeController.cs
@@ -1,6 +1,11 @@
-﻿using System;
+﻿using DayThree_Project.App_Start;
+using DayThree_Project.Models;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
+using System.Net.Mail;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -22,9 +27,44 @@ namespace DayThree_Project.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            EmailModel model = new EmailModel();
 
-            return View();
+            return View(model);
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Contact(EmailModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var body = "<p>Email From: <bold>{0}</bold>({1})</p><p>Message:</p><p>{2}</p>";
+                    var from = model.FromEmail;
+                    model.Body = model.Body;
+        
+                    var email = new MailMessage(from, ConfigurationManager.AppSettings["emailto"])
+                    {
+                        Subject = model.Subject,
+                        Body = string.Format(body, model.FromName, model.FromEmail,
+                                             model.Body),
+                        IsBodyHtml = true
+                    };
+
+                    var svc = new PersonalEmail();
+                    await svc.SendAsync(email);
+
+                    return View("Index");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    await Task.FromResult(0);
+                }
+            }
+            return View(model);
+        }
+
     }
 }
---
+++ DayThree_Project/Models/EmailModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace DayThree_Project.Models
+{
+    public class EmailModel
+    {
+            [Required, Display(Name = "Name")]
+            public string FromName { get; set; }
+            [Required, Display(Name = "Email"), EmailAddress]
+            public string FromEmail { get; set; }
+            [Required]
+            public string Subject { get; set; }
+            [Required, DataType(DataType.MultilineText)]
+            public string Body { get; set; }
+    }
+}
